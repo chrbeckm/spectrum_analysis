a2a94efc11235701e8fd8c290273d95ed50e5a5f
hmoldenhauer <henning.moldenhauer@udo.edu>
2018-07-23 17:24:21 +0200

    do not use tripple quotes for comments

--- methods/analyze.py
+++ methods/analyze.py
@@ -1,4 +1,4 @@
-from ramanspectrum import *
+from functions import *
 import sys
 import time
 
@@ -9,11 +9,13 @@ def analyze(label):
     Combination of the methods provided by ramanspectrum class.
     """
 
-    spec = ramanspectrum(label + '/data_' + label + '.txt', label = label)
-    spec.SelectSpectrum()
-    spec.SelectBaseline()
-    spec.SelectPeaks()
-    spec.FitSpectrum()
+    x, y, maxyvalue = initialize(label + '/data_' + label + '.txt')
+    print(maxyvalue)
+    #spec = ramanspectrum(label + '/data_' + label + '.txt', label = label)
+    #spec.SelectSpectrum()
+    #spec.SelectBaseline()
+    #spec.SelectPeaks()
+    #spec.FitSpectrum()
 
 
 if __name__ == '__main__':
