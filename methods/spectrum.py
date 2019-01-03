26652592d0392ef580b9e46ebe8029f31d3f7631
Stefan <stefan.grisard@tu-dortmund.de>
2019-01-03 15:45:40 +0100

    enable deselecting a selected peak by right mouse click

--- methods/spectrum.py
+++ methods/spectrum.py
@@ -464,7 +464,7 @@ class spectrum(object):
             self.FitBaseline(spectrum=i, show=show, degree=degree)
 
     # function that plots the dots at the peaks you wish to fit
-    def PlotPeaks(self, fig):
+    def PlotPeaks(self, fig, ax):
         """
         Plot the selected peaks while :func:`~spectrum.SelectPeaks` is running.
 
@@ -477,14 +477,29 @@ class spectrum(object):
         """
         xpeak = []  # x and
         ypeak = []  # y arrays for peak coordinates
+        global line
+        line, = ax.plot(xpeak, ypeak, 'ro', markersize = 10)
+        
 
         def onclickpeaks(event):
-            if event.button:
+            
+            if event.button == 1: # left mouse click to add data point
                 xpeak.append(event.xdata)               # append x data and
                 ypeak.append(event.ydata)               # append y data
-                plt.plot(event.xdata, event.ydata, 'ro',# plot the selected peak
-                        picker=5)
-                fig.canvas.draw()                       # and show it
+                line.set_xdata(xpeak)
+                line.set_ydata(ypeak)        
+                plt.draw()                       # and show it
+
+
+            if event.button == 3: #right mouse click to remove data point
+                if xpeak != []:
+                    xdata_nearest_index = ( np.abs(xpeak - event.xdata) ).argmin() #nearest neighbour  
+                    del xpeak[xdata_nearest_index] #delte x 
+                    del ypeak[xdata_nearest_index] #and y data point
+                    line.set_xdata(xpeak) #update x
+                    line.set_ydata(ypeak) #and y data in plot
+                    plt.draw()   # and show it    
+                    
 
         # actual execution of the defined function oneclickpeaks
         cid = fig.canvas.mpl_connect('button_press_event', onclickpeaks)
@@ -533,7 +548,7 @@ class spectrum(object):
                              Select the maxima of the ' + peaktype +\
                              '-PEAKS to fit.')
                 # arrays of initial values for the fits
-                xpeak, ypeak = self.PlotPeaks(fig)
+                xpeak, ypeak = self.PlotPeaks(fig, ax)
                 plt.show()
                 # store the chosen initial values
                 peakfile = self.folder + '/temp/locpeak_' + peaktype + '_' +\
