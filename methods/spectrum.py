ce1b9728a87a802ab67cb25934b0cd4bcc7a747f
Stefan <stefan.grisard@tu-dortmund.de>
2019-01-09 15:13:14 +0100

    warn if one value reaches fit boundaries

--- methods/spectrum.py
+++ methods/spectrum.py
@@ -647,11 +647,26 @@ class spectrum(object):
 
             # create the fit parameters of the background substracted fit
             pars = ramanmodel.make_params()
+            lower_bounds = [pars[key].min for key in pars.keys()] #lists of lower and upper bounds of the start parameters
+            upper_bounds = [pars[key].max for key in pars.keys()]
+            
             # fit the data to the created model
             self.fitresult_peaks[spectrum] = ramanmodel.fit(y_fit, pars,
                                                     x = self.xreduced[spectrum],
                                                     method = 'leastsq',
                                                     scale_covar = True)
+
+            best_values = list(self.fitresult_peaks[spectrum].best_values.values()) #best values of all parameters in the spectrum
+
+            
+            lower_mask = best_values == lower_bounds #mask = True if best_values has reached lower bound  
+            upper_mask = best_values == upper_bounds #mask = True if best_values has reached upper bound 
+
+            if True in lower_mask or True in upper_mask: #warn if one of the parameters has reached the bounds
+                warn(ParameterWarning)
+            
+                                                            
+                                                  
             # calculate the fit line
             self.fitline[spectrum] = ramanmodel.eval(
                                         self.fitresult_peaks[spectrum].params,
