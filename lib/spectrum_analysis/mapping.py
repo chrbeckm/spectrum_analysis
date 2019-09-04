951602f0c805abd4d8fe4aa6633072b575cdd749
hmoldenhauer <henning.moldenhauer@udo.edu>
2019-09-04 18:01:08 +0200

    corrected fontsizes

--- lib/spectrum_analysis/mapping.py
+++ lib/spectrum_analysis/mapping.py
@@ -541,8 +541,9 @@ class mapping(spectrum):
 
         clb.set_label(label
                       + '(10$^{:1.0f}$ '.format(max_exp)
-                      + unit + ')')
+                      + unit + ')', fontsize='small')
         clb.ax.set_yticklabels('{:1.2f}'.format(x) for x in tickvalues)
+        clb.ax.tick_params(labelsize='small')
 
     def CreatePlotValues(self, maptype, y, **kwargs):
         """
