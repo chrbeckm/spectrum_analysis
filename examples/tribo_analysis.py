dd364b254feaa5ebd96343454db5809449a5f07c
hmoldenhauer <henning.moldenhauer@udo.edu>
2019-11-19 14:06:06 +0100

    added error calculation

--- examples/tribo_analysis.py
+++ examples/tribo_analysis.py
@@ -13,36 +13,46 @@ datapoints = 100
 
 linebreaker ='============================================================'
 
-start = []
-stop = []
+start = {}
+stop = {}
 
-folder = f'{tribofolder}/results/tribo'
-if not os.path.exists(folder):
-    os.makedirs(folder)
+resfolder = f'{tribofolder}/results/tribo'
+if not os.path.exists(resfolder):
+    os.makedirs(resfolder)
 
-startfile = f'{folder}/start_mu.dat'
-stopfile = f'{folder}/stop_mu.dat'
+startfile = f'{resfolder}/start_mu.dat'
+stopfile = f'{resfolder}/stop_mu.dat'
 
 def find_nearest_index(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
 
-def save_mu(file, namelist, valuelist):
+def update_dict(dict, key, array):
+    mean = np.mean(array)
+    err = np.std(array)
+    content = {key : {'value' : mean, 'error': err}}
+    dict.update(content)
+
+def save_mu(file, dict):
     with open(file, 'w') as f:
-        for i, value in enumerate(valuelist):
-            savename = namelist[i].split('/')[-1]
-            savename = savename.split('.')[0]
-            f.write(f'{savename}\t{value}\n')
+        for key in dict.keys():
+            value = dict[key]['value']
+            error = dict[key]['error']
+            f.write(f'{key}\t{value:13.5f} +/- {error:11.5f}\n')
 
 print(f'{linebreaker}\n{linebreaker}')
 
 for datafile in datafiles:
     index = datafiles.index(datafile)
+    savename = datafile.split('/')[-1]
+    savename = savename.split('.')[0]
+
     print(f'Data set {index + 1} of {datasets} is analyzed.')
+    print(f'{datafile} is analyzed.')
     x, y = GetData(datafile,
                    measurement='tribo',
-                   xdata='Time',
+                   xdata='Distance',
                    ydata='µ')
 
     laps, cycle = GetData(datafile,
@@ -51,26 +61,32 @@ for datafile in datafiles:
                           ydata='Cycle')
 
     # calculate nearest index to running_in/_out laps
-    # and get means of first and last number of datapoints
     f_idx = find_nearest_index(laps, running_in)
-    first = np.mean(y[f_idx : (f_idx + datapoints)])
-    start.append(first)
+    y_temp = y[f_idx : (f_idx + datapoints)]
+    update_dict(start, savename, y_temp)
 
     l_idx = find_nearest_index(abs(laps - laps[-1]), running_out)
-    last = np.mean(y[-l_idx : -running_out])
-    stop.append(last)
+    y_temp = y[-l_idx : -running_out]
+    update_dict(stop, savename, y_temp)
 
     print(f'Median of the first {datapoints} points after running in '
-          f'for {laps[f_idx]} laps:\t{first}')
+          f'for {laps[f_idx]} laps:\t{start[savename]["value"]:.5f}')
     print(f'Median of the last {datapoints} points before running out '
-          f'for {laps[-1]-laps[l_idx]} laps:\t{last}')
+          f'for {laps[-1]-laps[l_idx]} laps:\t{stop[savename]["value"]:.5f}')
     print(linebreaker + '\n' + linebreaker)
 
-    # plot spectrum
-    #fig, ax = plt.subplots()
-    #ax.plot(x, y, 'b-', label='Data')
-    #
-    #plt.show()
+    # plot and save spectrum
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'b-', label='Data')
 
-save_mu(startfile, datafiles, start)
-save_mu(stopfile, datafiles, stop)
+    fig.legend(loc = 'lower right')
+    plt.ylabel('µ (arb. u.)')
+    plt.xlabel('Distance (m)')
+
+    # save figures
+    fig.savefig(f'{resfolder}/{savename}_plot.pdf')
+    fig.savefig(f'{resfolder}/{savename}_plot.png', dpi=300)
+    plt.close()
+
+save_mu(startfile, start)
+save_mu(stopfile, stop)
