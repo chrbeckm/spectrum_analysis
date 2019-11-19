20f2422bcab2e7dd042a2cd40a58769b47bee438
hmoldenhauer <henning.moldenhauer@udo.edu>
2019-11-19 12:00:03 +0100

    added multiple mu calculation

--- examples/tribo_analysis.py
+++ examples/tribo_analysis.py
@@ -1,41 +1,75 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-from spectrum_analysis.data import GetData
+from spectrum_analysis.data import GetData, GetFolderContent
 
-datafile = 'testdata/tribo_test.txt'
+datafiles, datasets = GetFolderContent('testdata/tribo', 'txt')
 running_in = 100
 running_out = 10
 datapoints = 100
+
+linebreaker ='============================================================'
+
+start = []
+stop = []
+
+folder = 'testdata/tribo/results/tribo'
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+startfile = f'{folder}/start_mu.dat'
+stopfile = f'{folder}/stop_mu.dat'
 
 def find_nearest_index(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
 
-x, y = GetData(datafile,
-               measurement='tribo',
-               xdata='Time',
-               ydata='µ')
+def save_mu(file, namelist, valuelist):
+    with open(file, 'w') as f:
+        for i, value in enumerate(valuelist):
+            savename = namelist[i].split('/')[-1]
+            savename = savename.split('.')[0]
+            f.write(f'{savename}\t{value}\n')
 
-laps, cycle = GetData(datafile,
-                      measurement='tribo',
-                      xdata='laps',
-                      ydata='Cycle')
+print(f'{linebreaker}\n{linebreaker}')
 
-# calculate nearest index to running_in laps
-index = find_nearest_index(laps, running_in)
-first_100 = np.mean(y[index : (index + datapoints)])
+for datafile in datafiles:
+    index = datafiles.index(datafile)
+    print(f'Data set {index + 1} of {datasets} is analyzed.')
+    x, y = GetData(datafile,
+                   measurement='tribo',
+                   xdata='Time',
+                   ydata='µ')
 
-last_100 = np.mean(y[-(datapoints +  running_out) : -(running_out)])
+    laps, cycle = GetData(datafile,
+                          measurement='tribo',
+                          xdata='laps',
+                          ydata='Cycle')
 
-print(f'Median of the first {datapoints} points after running in '
-      f'for {laps[index]} laps:\t{first_100}')
-print(f'Median of the last {datapoints} points before running out '
-      f'for {laps[-1]-laps[-running_out]} laps:\t{last_100}')
+    # calculate nearest index to running_in/_out laps
+    # and get means of first and last number of datapoints
+    f_idx = find_nearest_index(laps, running_in)
+    first = np.mean(y[f_idx : (f_idx + datapoints)])
+    start.append(first)
 
-# plot spectrum
-#fig, ax = plt.subplots()
-#ax.plot(x, cycle, 'b-', label='Data')
-#
-#plt.show()
+    l_idx = find_nearest_index(abs(laps - laps[-1]), running_out)
+    last = np.mean(y[-l_idx : -running_out])
+    stop.append(last)
+
+    print(f'Median of the first {datapoints} points after running in '
+          f'for {laps[f_idx]} laps:\t{first}')
+    print(f'Median of the last {datapoints} points before running out '
+          f'for {laps[-1]-laps[l_idx]} laps:\t{last}')
+    print(linebreaker + '\n' + linebreaker)
+
+    # plot spectrum
+    #fig, ax = plt.subplots()
+    #ax.plot(x, y, 'b-', label='Data')
+    #
+    #plt.show()
+
+save_mu(startfile, datafiles, start)
+save_mu(stopfile, datafiles, stop)
