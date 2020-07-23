fix memory leakage

examples/map_plot_tester.py
@@ -8,6 +8,15 @@ from spectrum_analysis import mapping as mp
 from spectrum_analysis import data
 from peaknames import *
 
+import pandas as pd
+import tracemalloc
+from pympler import muppy, summary
+
+debug = False
+
+if debug:
+    tracemalloc.start()
+
 mapFolderList = [os.path.join('testdata', '1'),
                  os.path.join('testdata', '2')
                  ]
@@ -24,8 +33,8 @@ backgrounds = ['bg_test.png',
                'bg_test.jpg'
               ]
 
-msizes = [2.0,
-          2.0
+msizes = [1.04,
+          1.04,
 ]
 
 # number of bins
@@ -75,13 +84,13 @@ def PlotParameterMappings(params, peakList, mapdims, step, background='',
         if dicti is not None:
             vmin = dicti[mapping][0]
             vmax = dicti[mapping][1]
-        plot_matrix, plotname = map.PlotMapping('params',
+        plot_matrix, plotname = mapp.PlotMapping('params',
                         params[i], mapdims, step,
                         name=name + mapping,
                         vmin=vmin, vmax=vmax, grid=False)
         plot_mats.append(plot_matrix)
         plot_nams.append(plotname)
-        map.PlotMapping('params', params[i], mapdims, step,
+        mapp.PlotMapping('params', params[i], mapdims, step,
                         name=name + 'grid_' + mapping, alpha=0.75,
                         vmin=vmin, vmax=vmax, grid=True,
                         background=background, msize=msize,
@@ -99,7 +108,7 @@ def PlotParameterMappings(params, peakList, mapdims, step, background='',
             if key in keys:
                 # get index of parameter and corresponding min and max
                 i = peakList.index(keys)
-                nonMissing = [x for x in params[i] if not (x == map.missingvalue)]
+                nonMissing = [x for x in params[i] if not (x == mapp.missingvalue)]
                 minval = np.min(nonMissing)
                 maxval = np.max(nonMissing)
                 if plot_ranges[key]['min'] is None:
@@ -114,7 +123,7 @@ def PlotParameterMappings(params, peakList, mapdims, step, background='',
 
     # plot histograms with the same plot ranges
     for mat, nam in zip(plot_mats, plot_nams):
-        map.PlotHistogram(mat, nam, bins=bins,
+        mapp.PlotHistogram(mat, nam, bins=bins,
                           rng=(plot_ranges[nam.split('_')[-1]]['min'],
                                plot_ranges[nam.split('_')[-1]]['max']))
 
@@ -124,8 +133,8 @@ def PlotErrorMappings(params, errors, peakList, mapdims, step):
     """
     for i, mapping in enumerate(peakList):
         # calculate relative error, set missing values and plot errors
-        relative_error = map.ModifyValues(errors[i], params[i], 'div')
-        map.PlotMapping('errs', relative_error, mapdims, step, name=mapping,
+        relative_error = mapp.ModifyValues(errors[i], params[i], 'div')
+        mapp.PlotMapping('errs', relative_error, mapdims, step, name=mapping,
                         numbered=True)
 
 def PlotParameterOperations(params, peakList, mapdims, step,
@@ -136,23 +145,23 @@ def PlotParameterOperations(params, peakList, mapdims, step,
     """
     a = peakList.index(first)
     b = peakList.index(second)
-    ratio = map.ModifyValues(params[a], params[b], operation)
+    ratio = mapp.ModifyValues(params[a], params[b], operation)
     filename = first + '_' + operation + '_' + second
     vmin = None
     vmax = None
     if dicti is not None:
         vmin = dicti[filename][0]
         vmax = dicti[filename][1]
-    plot_matrix, plotname = map.PlotMapping(operation,
+    plot_matrix, plotname = mapp.PlotMapping(operation,
                     ratio, mapdims, step,
                     name=name + filename,
                     numbered=False, vmin=vmin, vmax=vmax, grid=False)
-    map.PlotMapping(operation, ratio, mapdims, step,
+    mapp.PlotMapping(operation, ratio, mapdims, step,
                     name=name + 'grid_' + filename, alpha=0.75,
                     numbered=False, vmin=vmin, vmax=vmax, grid=True,
                     background=background, msize=msize,
                     plot_missing=False, area=area)
-    map.PlotHistogram(plot_matrix, plotname, bins=bins)
+    mapp.PlotHistogram(plot_matrix, plotname, bins=bins)
     return filename, ratio
 
 def CreateMinMaxDict(params, paramList, mapping):
@@ -164,7 +173,7 @@ def CreateMinMaxDict(params, paramList, mapping):
     for param in paramList:
         # get index of parameter and corresponding min and max
         i = paramList.index(param)
-        nonMissing = [x for x in params[i] if not (x == map.missingvalue)]
+        nonMissing = [x for x in params[i] if not (x == mapp.missingvalue)]
         min = np.min(nonMissing)
         max = np.max(nonMissing)
         minfile = mapping
@@ -192,7 +201,7 @@ def UpdateGlobalDict(globaldict, dicti):
             # check if parameter smaller/bigger than current value
             # and update values and mappings
             if ((globaldict[param][0] > min)
-            and not (globaldict[param][0] == map.missingvalue)):
+            and not (globaldict[param][0] == mapp.missingvalue)):
                 pass
             else:
                 min = globaldict[param][0]
@@ -231,23 +240,23 @@ for folder in mapFolderList:
     background = folder + os.sep + backgrounds[index]
     msize = msizes[index]
 
-    map = mp.mapping(foldername=folder, plot=True, peaknames=peaknames)
+    mapp = mp.mapping(foldername=folder, plot=True, peaknames=peaknames)
 
     # get and plot raw data
-    x, y = data.GetAllData(map.listOfFiles)
-    map.PlotMapping('raw', y, mapdims, step, x=x, xmin=1300, xmax=1400)
+    x, y = data.GetAllData(mapp.listOfFiles)
+    mapp.PlotMapping('raw', y, mapdims, step, x=x, xmin=1300, xmax=1400)
     if plotrawspectra:
-        map.PlotAllRawSpectra(x, y)
+        mapp.PlotAllRawSpectra(x, y)
 
     # plot all colormaps
-    #map.PlotAllColormaps('raw', y, mapdims, step, x=x, xmin=1300, xmax=1400)
+    #mapp.PlotAllColormaps('raw', y, mapdims, step, x=x, xmin=1300, xmax=1400)
 
     # get fit data and plot one map per peak parameter
-    peakFileList, numberOfPeakFiles = data.GetFolderContent(map.pardir_peak,
+    peakFileList, numberOfPeakFiles = data.GetFolderContent(mapp.pardir_peak,
                                                         filetype='dat',
                                                         objects='peakparameter')
     parameters, errors = data.GetAllData(peakFileList)
-    parameterList = map.CreatePeakList(peakFileList)
+    parameterList = mapp.CreatePeakList(peakFileList)
 
     # calculate area under the curve
     # area is used to scale the linewidth of the grid marker
@@ -256,10 +265,13 @@ for folder in mapFolderList:
         test = re.findall('amplitude', parameter)
         if test:
             area += parameters[i]
-
+    if debug:
+        time1 = tracemalloc.take_snapshot()
     PlotParameterMappings(parameters, parameterList, mapdims, step,
                           background=background, msize=msize, area=area)
     PlotErrorMappings(parameters, errors, parameterList, mapdims, step)
+    if debug:
+        time2 = tracemalloc.take_snapshot()
 
     dict_minmax = CreateMinMaxDict(parameters, parameterList, folder)
     dict_minmax_global = UpdateGlobalDict(dict_minmax_global, dict_minmax)
@@ -292,11 +304,11 @@ for folder in mapFolderList:
 
     # plot background values from fits
     if bg_plot:
-        peakFileList, numberOfPeakFiles = data.GetFolderContent(map.pardir_peak_bg,
+        peakFileList, numberOfPeakFiles = data.GetFolderContent(mapp.pardir_peak_bg,
                                                             filetype='dat',
                                                             quiet=True)
         parameters, errors = data.GetAllData(peakFileList)
-        parameterList_bg = map.CreatePeakList(peakFileList)
+        parameterList_bg = mapp.CreatePeakList(peakFileList)
         PlotParameterMappings(parameters, parameterList_bg, mapdims, step,
                               background=background, msize=msize, area=area)
 
@@ -307,6 +319,19 @@ for folder in mapFolderList:
     PrintMinMax(dict_minmax, parameterList)
 
     print(linebreaker + '\n' + linebreaker)
+
+    if debug:
+        all_objects = muppy.get_objects()
+        sum1 = summary.summarize(all_objects)# Prints out a summary of the large objects
+        summary.print_(sum1)# Get references to certain types of objects such as dataframe
+        dataframes = [ao for ao in all_objects if isinstance(ao, pd.DataFrame)]
+
+        for dat in dataframes:
+            print(dat.columns.values)
+            print(len(dat))
+        stats = time2.compare_to(time1, 'lineno')
+        for stat in stats[:10]:
+            print(stat)
 
 if scaled:
     print('List of global minima and maxima '
@@ -324,14 +349,14 @@ if scaled:
         background = folder + os.sep + backgrounds[index]
         msize = msizes[index]
 
-        map = mp.mapping(foldername=folder, plot=True, peaknames=peaknames)
+        mapp = mp.mapping(foldername=folder, plot=True, peaknames=peaknames)
 
         # get fit data and plot one map per peak parameter
-        peakFileList, numberOfPeakFiles = data.GetFolderContent(map.pardir_peak,
+        peakFileList, numberOfPeakFiles = data.GetFolderContent(mapp.pardir_peak,
                                                         filetype='dat',
                                                         objects='peakparameter')
         parameters, errors = data.GetAllData(peakFileList)
-        parameterList = map.CreatePeakList(peakFileList)
+        parameterList = mapp.CreatePeakList(peakFileList)
         PlotParameterMappings(parameters, parameterList, mapdims, step,
                               background=background, msize=msize,
                               name='scaled_',

lib/spectrum_analysis/mapping.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import numpy as np
 
@@ -21,6 +22,8 @@ from mpl_toolkits import axes_grid1
 This module contains the mapping class to work multiple x, y structured
 data sets.
 """
+
+matplotlib.use('Agg')  # might need adjustment in case of memory leakage
 
 class scatter():
     def __init__(self, x, y, ax, msize=1, area=None, **kwargs):
@@ -77,7 +80,7 @@ class scatter():
     def mscatter(self, x, y, ax=None, m=None, **kwargs):
         import matplotlib.markers as mmarkers
         if not ax: ax=plt.gca()
-        sc = ax.scatter(x,y,**kwargs)
+        scs = ax.scatter(x,y,**kwargs)
         if (m is not None):# and (len(m)==len(x)):
             paths = []
             for marker in m:
@@ -88,8 +91,8 @@ class scatter():
                 path = marker_obj.get_path().transformed(
                             marker_obj.get_transform())
                 paths.append(path)
-            sc.set_paths(paths)
-        return sc
+            scs.set_paths(paths)
+        return scs
 
 class mapping(spectrum):
     """
@@ -751,7 +754,7 @@ class mapping(spectrum):
                                                     facecolor=facecolor)
         return pc
 
-    def ConfigureTicks(self, mapdims, step, xticker, plt, grid, remove=2):
+    def ConfigureTicks(self, mapdims, step, xticker, plot, grid, remove=2):
         xdim = mapdims[0]
         ydim = mapdims[1]
         # create x and y ticks accordingly to the parameters of the mapping
@@ -761,28 +764,28 @@ class mapping(spectrum):
             y_ticks = y_ticks[::-1]
 
 
-        plt.xticks(np.arange(xdim, step=xticker), x_ticks, fontsize='small')
-        plt.yticks(np.arange(ydim), y_ticks, fontsize='small')
+        plot.xticks(np.arange(xdim, step=xticker), x_ticks, fontsize='small')
+        plot.yticks(np.arange(ydim), y_ticks, fontsize='small')
 
-        ax = plt.gca()
-        plt.setp(ax.xaxis.get_ticklabels()[1::remove], visible=False)
+        ax = plot.gca()
+        plot.setp(ax.xaxis.get_ticklabels()[1::remove], visible=False)
         if grid:
-            plt.setp(ax.yaxis.get_ticklabels()[1::remove], visible=False)
+            plot.setp(ax.yaxis.get_ticklabels()[1::remove], visible=False)
         else:
             if mapdims[1] % 2 == 0:
-                plt.setp(ax.yaxis.get_ticklabels()[0::remove], visible=False)
+                plot.setp(ax.yaxis.get_ticklabels()[0::remove], visible=False)
             else:
-                plt.setp(ax.yaxis.get_ticklabels()[1::remove], visible=False)
+                plot.setp(ax.yaxis.get_ticklabels()[1::remove], visible=False)
 
-    def ConfigurePlot(self, clb, peak, **kwargs):
+    def ConfigurePlot(self, clb, plot, peak, **kwargs):
         # set title, label of x, y and z axis
         #plt.title('Mapping of ' + self.folder + ' ' + peak, fontsize='small')
-        plt.ylabel('y-Position ($\mathrm{\mu}$m)', fontsize='small')
-        plt.xlabel('x-Position ($\mathrm{\mu}$m)', fontsize='small')
+        plot.ylabel('y-Position ($\mathrm{\mu}$m)', fontsize='small')
+        plot.xlabel('x-Position ($\mathrm{\mu}$m)', fontsize='small')
         self.LabelZ(clb, **kwargs)
 
         # have a tight layout
-        plt.tight_layout()
+        plot.tight_layout()
 
     def PlotMapping(self, maptype, y, mapdims, step, area=None,
                     xticker=1, colormap='Reds', alpha=1.0,
@@ -884,6 +887,7 @@ class mapping(spectrum):
                 plt.imshow(img, zorder=0, cmap='Greys_r',
                            extent=[0+pos, mapdims[0]+pos,
                                    0+pos, mapdims[1]+pos])
+                del img
             except ValueError:
                 #traceback.print_exc()
                 print('No background given.')
@@ -895,6 +899,7 @@ class mapping(spectrum):
             sclb = scatter(x, y, ax, c=plot_vector, msize=msize, area=area_corr,
                            cmap='Reds', linewidth=0.5, alpha=alpha)
             im = sclb.sc
+            del sclb
         else:
             im = plt.imshow(plot_matrix, cmap=colormap, vmin=vmin, vmax=vmax)
 
@@ -909,7 +914,8 @@ class mapping(spectrum):
             current_ax = plt.gca()
             cax = divider.append_axes("right", size='5%', pad=0.05)
             plt.sca(current_ax)
-            return im.axes.figure.colorbar(im, cax=cax, **kwargs)
+            cbar = im.axes.figure.colorbar(im, cax=cax, **kwargs)
+            return cbar
 
         clb = add_colorbar(im)
 
@@ -921,7 +927,7 @@ class mapping(spectrum):
                     color = 'black'
                     if missing_matrix[j][mapdims[0] - i-1]:
                         color = 'white'
-                    text = ax.text(mapdims[0] - i-1, j,
+                    ax.text(mapdims[0] - i-1, j,
                                    product - (j * mapdims[0] + i),
                                    ha='center', va='center',
                                    color=color, fontsize=fontsize_int*0.4)
@@ -958,13 +964,16 @@ class mapping(spectrum):
         if maptype == 'errs':
             zlabel = 'Relative error of\n' + zlabel
 
-        self.ConfigurePlot(clb,
+        self.ConfigurePlot(clb, plt,
                            peak = peakshape,
                            label = zlabel,
                            unit = unit)
         plt.savefig(savefile + '_' + colormap + '.pdf', format='pdf')
         plt.savefig(savefile + '_' + colormap + '.png')
-        plt.close()
+        plt.clf()
+        plt.close(fig)
+        del im, clb
+        gc.collect()
 
         print(plotname + ' ' + colormap + ' plotted')
 
@@ -989,6 +998,7 @@ class mapping(spectrum):
         label = self.peaknames[peakshape][peakparameter]['name']
         unit = self.peaknames[peakshape][peakparameter]['unit']
 
+        fig, ax = plt.subplots()
         plt.hist(plot_values, bins=bins, range=rng)
 
         self.FormatxLabelAndTicks(plt, name=label, unit=unit)
@@ -998,6 +1008,7 @@ class mapping(spectrum):
 
         plt.savefig(os.path.join(self.folder, 'results', 'plot',
                                  f'hist_{plotname}.png'), dpi=300)
+        fig.clf()
         plt.close()
 
     def PlotAllColormaps(self, maptype, y, mapdims, step, **kwargs):