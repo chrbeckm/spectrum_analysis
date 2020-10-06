76e762c8b13361500adc7705a14511eceaa7ac17
hmoldenhauer <henning.moldenhauer@udo.edu>
2020-10-06 13:40:14 +0200

    add lubricant and atmosphere to clustering

--- examples/pca_global_analysis.py
+++ examples/pca_global_analysis.py
@@ -43,7 +43,9 @@ mappings = {
             'markersize': 1.04,
             'n_clusters': 4,
             'temperature': 100,
-            'friction': 1},
+            'friction': 1,
+            'lubricant': 'no oil',
+            'atmosphere': 'air'},
     '002': {'mapfolder': os.path.join('testdata', '2'),
             'dims': (8, 2),
             'stepsize': 10,
@@ -51,7 +53,9 @@ mappings = {
             'markersize': 1.04,
             'n_clusters': 3,
             'temperature': 200,
-            'friction': 0.5},
+            'friction': 0.5,
+            'lubricant': 'base oil',
+            'atmosphere': 'argon'},
     '003': {'mapfolder': os.path.join('testdata', '3'),
             'dims': (4, 4),
             'stepsize': 10,
@@ -59,7 +63,9 @@ mappings = {
             'markersize': 1.04,
             'n_clusters': 4,
             'temperature': 300,
-            'friction': 0.7},
+            'friction': 0.7,
+            'lubricant': 'corn oil',
+            'atmosphere': 'nitrogen'},
     }
 
 prefix = 'global'
@@ -86,6 +92,10 @@ plot_only_dirs = {'fwhm': {'linestyle': '-',
                                'plot_label': True},
                   'temperature': {'linestyle': '-.',
                                   'plot_label': True},
+                  'lubricant': {'linestyle': '-.',
+                                'plot_label': True},
+                  'atmosphere': {'linestyle': '-.',
+                                 'plot_label': True},
                   }
 clustering = 'SpectralClustering'  # SpectralClustering (or OPTICS)
 
@@ -144,18 +154,38 @@ if plot_parameter_directions:
 print(linebreaker + '\n' + linebreaker)
 
 # scale additional parameters and add to mappings dictionary
-additional_parameters = ['temperature', 'friction']
+additional_parameters = ['temperature', 'friction', 'atmosphere', 'lubricant']
+translation = {
+    'lubricant': {'no oil':       0.00,
+                  'dried oil':    0.20,
+                  'base oil':     0.25,
+                  'additive oil': 0.30,
+                  'corn oil':     0.50,
+                  'DEG':          0.70,
+                  'EG':           0.80,
+                  'silicone oil': 1.00},
+    'atmosphere': {'argon':    0.00,
+                   'air':      0.78,
+                   'nitrogen': 1.00}
+    }
 for param in additional_parameters:
-    values = [float(mappings[x][param]) for x in mappings.keys()]    
+    if type(list(mappings.items())[0][1][param]) == str:
+        valuenames = [mappings[x][param] for x in mappings.keys()]
+        values = [translation[param][str(name)] for name in valuenames]
+    else:
+        values = [float(mappings[x][param]) for x in mappings.keys()]
     scaled_temp = scaleParameters(np.array([values]))
     keylist = list(mappings.keys())
     for key in keylist:
         idx = keylist.index(key)
         mappings[key][f'scaled_p1_{param}'] = scaled_temp[0][idx]
 
-global_transposed = np.zeros((1, max_fitting_parameters + len(additional_parameters)))
-global_parameters = np.zeros((max_fitting_parameters + len(additional_parameters), 1))
-global_errors = np.zeros((max_fitting_parameters + len(additional_parameters), 1))
+global_transposed = np.zeros((1, max_fitting_parameters
+                              + len(additional_parameters)))
+global_parameters = np.zeros((max_fitting_parameters
+                              + len(additional_parameters), 1))
+global_errors = np.zeros((max_fitting_parameters
+                          + len(additional_parameters), 1))
 global_parameterList = []
 global_spectraList = []
 for key in mappings.keys():
@@ -164,7 +194,7 @@ for key in mappings.keys():
     print(f'{folder} mappings are plotted now.')
     mapdims = mappings[key]['dims']
     step = mappings[key]['stepsize']
-    background = folder + os.sep + mappings[key]['background']
+    background = f'{folder}{os.sep}{mappings[key]["background"]}'
     msize = mappings[key]['markersize']
 
     mapp = mp.mapping(foldername=folder, plot=True, peaknames=peaknames)
@@ -185,7 +215,8 @@ for key in mappings.keys():
     if len(scaled_parameters) < max_fitting_parameters:
         while max_fitting_parameters > len(scaled_parameters):
             scaled_parameters = np.append(scaled_parameters,
-                                          np.zeros((1, numberOfSpectra)), axis=0)
+                                          np.zeros((1, numberOfSpectra)),
+                                          axis=0)
             parameters = np.append(parameters,
                                    np.zeros((1, numberOfSpectra)), axis=0)
             errors = np.append(errors,
@@ -196,11 +227,13 @@ for key in mappings.keys():
     for param in additional_parameters:
         parametername = f'scaled_p1_{param}'
         scaled_parameters = np.append(scaled_parameters,
-                                      np.ones((1, numberOfSpectra)) * mappings[key][parametername], axis=0)
+                                      np.ones((1, numberOfSpectra))
+                                      * mappings[key][parametername], axis=0)
         parameters = np.append(parameters,
-                               np.ones((1, numberOfSpectra)) * mappings[key][parametername], axis=0)
+                               np.ones((1, numberOfSpectra))
+                               * mappings[key][parametername], axis=0)
         errors = np.append(errors,
-                               np.zeros((1, numberOfSpectra)), axis=0)
+                           np.zeros((1, numberOfSpectra)), axis=0)
         parameterList.append(parametername)
 
     # transpose data, so all parameters of one spectrum are in one array
@@ -235,6 +268,7 @@ if print_PCA_results:
     printPCAresults(pca, parameterList,
                     print_components=print_PC_components)
 
+print('PCA generated.')
 # plot everything and annotate each datapoint
 fig = plt.figure()
 ax_sum = [plt.subplot2grid((4, 3), (0, 2)),
@@ -249,6 +283,7 @@ PC = np.vstack((x, y)).transpose()
 cluster, sc, PC = createCluster(clustering, PC, n_clusters,
                                 pointsize=pointsize)
 
+print('Clusters created.')
 # create ranked PC clusters
 PC_ranked, newlabels = createRankedClusters(PC, cluster.labels_)
 
@@ -270,6 +305,9 @@ for i, clust in enumerate(PC_ranked):
             addPoint(sc, point, colors[i], white=white)
         else:
             addPoint(sc, point, colors[i])
+    print(f'Cluster {i} plotted.')
+
+print('All clusters plotted.')
 
 # get four biggest clusters and plot the sum spectrum
 plotClusterOverview(global_spectraList,
@@ -454,7 +492,7 @@ else:
     plt.savefig(f'{global_spectraList[0].split(os.sep)[0]}{os.sep}'
                 f'{prefix}_pca_analysis'
                 f'_pc{component_x}_pc{component_y}.pdf')
-ax.set_title(f'PCA Analysis of {folder}')
+ax.set_title(f'Global PCA Analysis')
 
 if show_hover_plot:
     plt.show()
